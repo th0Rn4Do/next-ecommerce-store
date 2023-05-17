15e8ae86bf443836bfa9ee25962292347a8d920c
Mandla <127042808+th0Rn4Do@users.noreply.github.com>
2023-05-17 15:08:27 +0200

    Implement font for the whole webpage in layout.js

--- app/layout.js
+++ app/layout.js
@@ -5,7 +5,10 @@ import Link from 'next/link';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
-  title: 'Next - Upleveled - Ecommerce project',
+  title: {
+    default: 'Next - Upleveled - Ecommerce project',
+    template: '%S | NEXT',
+  },
   description:
     'This is the descripton of the ecommerce project which eventually will show up in a search engine.',
 };
@@ -13,7 +16,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
+      <body className={inter.className}>
         <nav>
           <Link href="/">Home</Link> <Link href="/about">About</Link>{' '}
           <Link href="/webshop">Webshop</Link>
