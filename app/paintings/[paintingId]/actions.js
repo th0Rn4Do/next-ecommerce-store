b32f3efd1490e2e0ebcaa9182547d151416bfa0d
Mandla <127042808+th0Rn4Do@users.noreply.github.com>
2023-05-29 23:39:51 +0200

    Add actions like Jose showed us but adapted to paintings

--- app/paintings/[paintingId]/actions.js
+++ app/paintings/[paintingId]/actions.js
@@ -1,4 +1,25 @@
 import { cookies } from 'next/headers';
 import { getCookie } from '../../../util/cookies';
+import { parseJson } from '../../../util/json';
 
-getCookie();
+export async function createOrUpdateQuantity(paintingId, quantity) {
+  const paintingQuantityCookie = getCookie('cart');
+
+  const paintingQuantities = !paintingQuantityCookie
+    ? []
+    : parseJson(paintingQuantityCookie);
+
+  const paintingToUpdate = paintingQuantities.find((paintingQuantity) => {
+    return paintingQuantity.id === paintingId;
+  });
+
+  if (paintingToUpdate) {
+    paintingToUpdate.quantity = quantity;
+  } else {
+    paintingQuantities.push({
+      id: paintingId,
+      quantity,
+    });
+  }
+  await cookies().set('cart', JSON.stringify(paintingQuantities));
+}
